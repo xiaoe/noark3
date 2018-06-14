8bc8261132c14530d4088bbd8fef1baea363db82
zhoumingkai <176543888@qq.com>
2018-06-14 17:03:51 +0800

    初始化Game工程

---
+++ noark-game/src/main/java/xyz/noark/game/bootstrap/DefualtServerBootstrap.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright © 2018 www.noark.xyz All Rights Reserved.
+ * 
+ * 感谢您选择Noark框架，希望我们的努力能为您提供一个简单、易用、稳定的服务器端框架 ！
+ * 除非符合Noark许可协议，否则不得使用该文件，您可以下载许可协议文件：
+ * 
+ * 		http://www.noark.xyz/LICENSE
+ *
+ * 1.未经许可，任何公司及个人不得以任何方式或理由对本框架进行修改、使用和传播;
+ * 2.禁止在本项目或任何子项目的基础上发展任何派生版本、修改版本或第三方版本;
+ * 3.无论你对源代码做出任何修改和改进，版权都归Noark研发团队所有，我们保留所有权利;
+ * 4.凡侵犯Noark版权等知识产权的，必依法追究其法律责任，特此郑重法律声明！
+ */
+package xyz.noark.game.bootstrap;
+
+import xyz.noark.core.bootstrap.AbstractServerBootstrap;
+import xyz.noark.core.network.PacketCodec;
+import xyz.noark.core.network.PacketCodecHolder;
+import xyz.noark.network.NettyServer;
+import xyz.noark.network.codec.json.SimpleJsonCodec;
+
+/**
+ * 一个默认的服务器启动引导类.
+ *
+ * @since 3.0
+ * @author 小流氓(176543888@qq.com)
+ */
+public class DefualtServerBootstrap extends AbstractServerBootstrap {
+
+	@Override
+	protected String getServerName() {
+		return "game-server";
+	}
+
+	@Override
+	protected void initNetworkService() {
+		PacketCodecHolder.setPacketCodec(getPacketCodec());
+		ioc.get(NettyServer.class).startup();
+	}
+
+	protected PacketCodec getPacketCodec() {
+		return new SimpleJsonCodec();
+	}
+}
---
+++ noark-game/src/main/java/xyz/noark/game/bootstrap/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 应用启动引导入口.
+ *
+ * @since 3.0
+ */
+package xyz.noark.game.bootstrap;
