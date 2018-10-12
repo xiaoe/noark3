f8a6052b796a486a3a8b45e809c1fc824164c179
zhoumingkai <176543888@qq.com>
2018-10-12 16:04:50 +0800

    Point类实现hashCode和equals方法

--- noark-core/src/main/java/xyz/noark/core/lang/Point.java
+++ noark-core/src/main/java/xyz/noark/core/lang/Point.java
@@ -66,6 +66,31 @@ public class Point {
 		this.y = y;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+
 	/**
 	 * 根据X与Y构建一个坐标点.
 	 * <p>
