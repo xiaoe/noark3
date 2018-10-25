dfb1834e7380f95b72cd320fd2c0374c8683441d
zhoumingkai <176543888@qq.com>
2018-10-25 21:17:33 +0800

    提供一批基本数据类型数组是否为空的判定

--- noark-core/src/main/java/xyz/noark/core/util/ArrayUtils.java
+++ noark-core/src/main/java/xyz/noark/core/util/ArrayUtils.java
@@ -60,6 +60,46 @@ public class ArrayUtils {
 		return !isEmpty(array);
 	}
 
+	public static boolean isEmpty(final boolean[] array) {
+		return array == null || array.length == 0;
+	}
+
+	public static boolean isNotEmpty(final boolean[] array) {
+		return !isEmpty(array);
+	}
+
+	public static boolean isEmpty(final int[] array) {
+		return array == null || array.length == 0;
+	}
+
+	public static boolean isNotEmpty(final int[] array) {
+		return !isEmpty(array);
+	}
+
+	public static boolean isEmpty(final long[] array) {
+		return array == null || array.length == 0;
+	}
+
+	public static boolean isNotEmpty(final long[] array) {
+		return !isEmpty(array);
+	}
+
+	public static boolean isEmpty(final float[] array) {
+		return array == null || array.length == 0;
+	}
+
+	public static boolean isNotEmpty(final float[] array) {
+		return !isEmpty(array);
+	}
+
+	public static boolean isEmpty(final double[] array) {
+		return array == null || array.length == 0;
+	}
+
+	public static boolean isNotEmpty(final double[] array) {
+		return !isEmpty(array);
+	}
+
 	public static int[] toIntArray(String[] array) {
 		return Arrays.stream(array).mapToInt(s -> Integer.parseInt(s)).toArray();
 	}
