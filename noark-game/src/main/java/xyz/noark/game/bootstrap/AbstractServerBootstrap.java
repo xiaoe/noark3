5969b1ee743e7e1cdd6e27cd0d2f1c4a67ccb033
zhoumingkai <176543888@qq.com>
2018-12-04 17:27:02 +0800

    修复PID存在时启服误删PID文件，必需手动删除

--- noark-game/src/main/java/xyz/noark/game/bootstrap/AbstractServerBootstrap.java
+++ noark-game/src/main/java/xyz/noark/game/bootstrap/AbstractServerBootstrap.java
@@ -130,7 +130,9 @@ public abstract class AbstractServerBootstrap implements ServerBootstrap {
 				}
 				// PID文件已存在...
 				else {
-					throw new ServerBootstrapException("PID文件已存在，如果异常停服，请手动删除PID文件 >> " + pidFileName);
+					final String fileName = pidFileName;
+					this.pidFileName = null;//
+					throw new ServerBootstrapException("PID文件已存在，如果异常停服，请手动删除PID文件 >> " + fileName);
 				}
 
 				// 写入PID
