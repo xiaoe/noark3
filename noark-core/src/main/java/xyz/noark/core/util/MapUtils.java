5f7fc568ca02d2e635c903cc41be398ee9eb7957
zhoumingkai <176543888@qq.com>
2019-01-19 14:43:03 +0800

    Map工具类增加一个获取最大Key的方法

--- noark-core/src/main/java/xyz/noark/core/util/MapUtils.java
+++ noark-core/src/main/java/xyz/noark/core/util/MapUtils.java
@@ -14,6 +14,7 @@
 package xyz.noark.core.util;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Map类型的工具类.
@@ -36,5 +37,39 @@ public class MapUtils {
 		HashMap<K, V> result = new HashMap<>(1, 1);
 		result.put(key, value);
 		return result;
+	}
+
+	/**
+	 * 从map中获取指定的Key，如果Key对应的值不存在，那就获取最大的Key所对应的值.
+	 * <p>
+	 * 常用于策划配置次数，比如获取20次所对应的资源，如果只配置到15，那就使用15对应的资源<br>
+	 * 1=10<br>
+	 * 2=15<br>
+	 * 3=20<br>
+	 * 4=25<br>
+	 * 5=30<br>
+	 * 10=100<br>
+	 * 如此配置，如果参数为6，应该返回是30，如果参数是12，应该返回100
+	 * 
+	 * @param map 配置Map
+	 * @param key 指定Key
+	 * @return 只要配置了值肯定会返回一个最近的值...
+	 */
+	public static <V> V getOrMaxKey(final Map<Integer, V> map, Integer key) {
+		V v = map.get(key);
+		if (v == null) {
+			// 遍历查找最优解...
+			int max = Integer.MIN_VALUE;
+			for (Map.Entry<Integer, V> e : map.entrySet()) {
+				if (v == null) {
+					v = e.getValue();
+					max = e.getKey().intValue();
+				} else if (e.getKey().intValue() > max && e.getKey().intValue() <= key.intValue()) {
+					v = e.getValue();
+					max = e.getKey().intValue();
+				}
+			}
+		}
+		return v;
 	}
 }
