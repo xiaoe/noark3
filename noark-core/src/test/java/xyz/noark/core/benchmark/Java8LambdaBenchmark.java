eae56de1be266fd76374fa6698df19c9d0a87bed
小流氓 <176543888@qq.com>
2019-10-12 15:23:49 +0800

    Java8Lambdafor性能测试

---
+++ noark-core/src/test/java/xyz/noark/core/benchmark/Java8LambdaBenchmark.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright © 2018 www.noark.xyz All Rights Reserved.
+ * 
+ * 感谢您选择Noark框架，希望我们的努力能为您提供一个简单、易用、稳定的服务器端框架 ！
+ * 除非符合Noark许可协议，否则不得使用该文件，您可以下载许可协议文件：
+ * 
+ * 		http://www.noark.xyz/LICENSE
+ *
+ * 1.未经许可，任何公司及个人不得以任何方式或理由对本框架进行修改、使用和传播;
+ * 2.禁止在本项目或任何子项目的基础上发展任何派生版本、修改版本或第三方版本;
+ * 3.无论你对源代码做出任何修改和改进，版权都归Noark研发团队所有，我们保留所有权利;
+ * 4.凡侵犯Noark版权等知识产权的，必依法追究其法律责任，特此郑重法律声明！
+ */
+package xyz.noark.core.benchmark;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import xyz.noark.benchmark.Benchmark;
+
+/**
+ * Java8Lambdafor性能测试
+ *
+ * @since 3.3.6
+ * @author 小流氓(176543888@qq.com)
+ */
+public class Java8LambdaBenchmark {
+	private final static Benchmark benchmark = new Benchmark();
+
+	public static void main(String[] args) throws Exception {
+		int max = 1_0000;
+		List<Integer> list = new ArrayList<>(max);
+		for (int i = 0; i < max; i++) {
+			list.add(i);
+		}
+
+		benchmark.doSomething("jdk7:", () -> jdk7(list));
+		benchmark.doSomething("jdk8:", () -> jdk8(list));
+	}
+
+	@SuppressWarnings("unused")
+	public static void jdk7(List<Integer> list) {
+		for (int i : list) {}
+	}
+
+	public static void jdk8(List<Integer> list) {
+		list.stream().forEach(v -> {});
+	}
+}
