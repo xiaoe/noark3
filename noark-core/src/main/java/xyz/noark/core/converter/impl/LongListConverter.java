f0d8d9be2ba0524d0b441554cc02447465ba17a9
小流氓 <176543888@qq.com>
2019-11-25 20:37:05 +0800

    添加一种Long类型的ArrayList

---
+++ noark-core/src/main/java/xyz/noark/core/converter/impl/LongListConverter.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright © 2018 www.noark.xyz All Rights Reserved.
+ * 
+ * 感谢您选择Noark框架，希望我们的努力能为您提供一个简单、易用、稳定的服务器端框架 ！
+ * 除非符合Noark许可协议，否则不得使用该文件，您可以下载许可协议文件：
+ * 
+ * 		http://www.noark.xyz/LICENSE
+ *
+ * 1.未经许可，任何公司及个人不得以任何方式或理由对本框架进行修改、使用和传播;
+ * 2.禁止在本项目或任何子项目的基础上发展任何派生版本、修改版本或第三方版本;
+ * 3.无论你对源代码做出任何修改和改进，版权都归Noark研发团队所有，我们保留所有权利;
+ * 4.凡侵犯Noark版权等知识产权的，必依法追究其法律责任，特此郑重法律声明！
+ */
+package xyz.noark.core.converter.impl;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Map;
+
+import xyz.noark.core.annotation.TemplateConverter;
+import xyz.noark.core.converter.AbstractConverter;
+import xyz.noark.core.lang.LongArrayList;
+import xyz.noark.core.lang.LongList;
+import xyz.noark.core.util.StringUtils;
+
+/**
+ * LongList转化器.
+ *
+ * @since 3.3.7
+ * @author 小流氓(176543888@qq.com)
+ */
+@TemplateConverter({ LongList.class, LongArrayList.class })
+public class LongListConverter extends AbstractConverter<LongList> {
+	@Override
+	public LongList convert(String value) {
+		if (StringUtils.isEmpty(value)) {
+			return new LongArrayList();
+		}
+
+		String[] array = StringUtils.split(value, ",");
+		LongList result = new LongArrayList(array.length);
+		Arrays.stream(array).forEach(v -> result.add(Integer.parseInt(v)));
+		return result;
+	}
+
+	@Override
+	public LongList convert(Field field, Map<String, String> data) {
+		if (data.isEmpty()) {
+			return new LongArrayList();
+		}
+		return new LongArrayList(data.values().stream().mapToLong(Long::parseLong).toArray());
+	}
+
+	@Override
+	public String buildErrorMsg() {
+		return "数字类型的数组应该是以英文逗号分隔的，如：1,2,3,4";
+	}
+}
