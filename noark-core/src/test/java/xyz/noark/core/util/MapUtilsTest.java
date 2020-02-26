9e38cffd95a4868e2611a53659238065c6a0bb52
小流氓 <176543888@qq.com>
2020-02-26 14:56:05 +0800

    编码标准的单元测试覆盖率的测试代码

--- noark-core/src/test/java/xyz/noark/core/util/MapUtilsTest.java
+++ noark-core/src/test/java/xyz/noark/core/util/MapUtilsTest.java
@@ -13,11 +13,16 @@
  */
 package xyz.noark.core.util;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -28,10 +33,40 @@ import org.junit.Test;
  */
 public class MapUtilsTest {
 
+	/**
+	 * 构建一个测试Map数据
+	 */
+	private Map<Integer, Integer> testMap = MapUtils.of(1, 2);
+	private Map<Integer, Long> ratioMap = new HashMap<>(16);
+
+	@Before
+	public void setUp() throws Exception {
+		ratioMap.put(1, 1000L);
+		ratioMap.put(2, 2000L);
+		ratioMap.put(3, 3000L);
+		ratioMap.put(4, 4000L);
+		ratioMap.put(5, 5000L);
+	}
+
+	@Test
+	public void testIsEmpty() {
+		assertTrue(MapUtils.isEmpty(null));
+		assertTrue(MapUtils.isEmpty(Collections.emptyMap()));
+		assertFalse(MapUtils.isEmpty(testMap));
+	}
+
+	@Test
+	public void testIsNotEmpty() {
+		assertFalse(MapUtils.isNotEmpty(null));
+		assertFalse(MapUtils.isNotEmpty(Collections.emptyMap()));
+		assertTrue(MapUtils.isNotEmpty(testMap));
+	}
+
 	@Test
 	public void testOf() {
-		Map<Integer, Integer> map = MapUtils.of(1, 1);
-		assertTrue(map.get(1) == 1);
+		assertEquals(null, testMap.get(2));
+		assertNotEquals(null, testMap.get(1));
+		assertEquals(Integer.valueOf(2), testMap.get(1));
 	}
 
 	@Test
@@ -43,20 +78,82 @@ public class MapUtilsTest {
 		map.put(4, 25);
 		map.put(5, 30);
 		map.put(10, 100);
-		assertTrue(MapUtils.getOrMaxKey(map, 1) == 10);
-		assertTrue(MapUtils.getOrMaxKey(map, 2) == 15);
-		assertTrue(MapUtils.getOrMaxKey(map, 4) == 25);
-		assertTrue(MapUtils.getOrMaxKey(map, 5) == 30);
-		assertTrue(MapUtils.getOrMaxKey(map, 6) == 30);
-		assertTrue(MapUtils.getOrMaxKey(map, 12) == 100);
+
+		assertEquals(MapUtils.getOrMaxKey(map, 1), Integer.valueOf(10));
+		assertEquals(MapUtils.getOrMaxKey(map, 2), Integer.valueOf(15));
+		assertEquals(MapUtils.getOrMaxKey(map, 4), Integer.valueOf(25));
+		assertEquals(MapUtils.getOrMaxKey(map, 5), Integer.valueOf(30));
+		assertEquals(MapUtils.getOrMaxKey(map, 6), Integer.valueOf(30));
+		assertEquals(MapUtils.getOrMaxKey(map, 12), Integer.valueOf(100));
 	}
 
 	@Test
-	public void testAddValue() {
-		Map<Integer, Integer> map = new HashMap<>(16);
-		assertTrue(MapUtils.addValue(map, 1, 0) == 0);
-		assertTrue(MapUtils.addValue(map, 1, 1) == 1);
-		assertTrue(MapUtils.addValue(map, 1, 1) == 2);
-		assertTrue(MapUtils.addValue(map, 1, 0) == 2);
+	public void testAddByIntValue() {
+		Map<Integer, Integer> source = MapUtils.of(1, 2);
+		MapUtils.addByIntValue(source, null);
+		MapUtils.addByIntValue(source, source);
+		assertEquals(source.get(1), Integer.valueOf(4));
+	}
+
+	@Test
+	public void testAddByLongValue() {
+		Map<Integer, Long> source = MapUtils.of(1, 2L);
+		MapUtils.addByLongValue(source, null);
+		MapUtils.addByLongValue(source, source);
+		assertEquals(source.get(1), Long.valueOf(4));
+	}
+
+	@Test
+	public void testAddValueMapOfInt() {
+		Map<Integer, Integer> source = new HashMap<>(16);
+		MapUtils.addValue(source, 1, 1);
+		assertEquals(source.get(1), Integer.valueOf(1));
+		MapUtils.addValue(source, 1, 1);
+		assertEquals(source.get(1), Integer.valueOf(2));
+	}
+
+	@Test
+	public void testAddValueMapOfLong() {
+		Map<Integer, Long> source = new HashMap<>(16);
+		MapUtils.addValue(source, 1, 1L);
+		assertEquals(source.get(1), Long.valueOf(1));
+		MapUtils.addValue(source, 1, 1L);
+		assertEquals(source.get(1), Long.valueOf(2));
+	}
+
+	@Test
+	public void testAddValueMapOfFloat() {
+		Map<Integer, Float> source = new HashMap<>(16);
+		MapUtils.addValue(source, 1, 1F);
+		assertEquals(source.get(1), Float.valueOf(1));
+		MapUtils.addValue(source, 1, 1F);
+		assertEquals(source.get(1), Float.valueOf(2));
+	}
+
+	@Test
+	public void testAddValueMapOfDouble() {
+		Map<Integer, Double> source = new HashMap<>(16);
+		MapUtils.addValue(source, 1, 1D);
+		assertEquals(source.get(1), Double.valueOf(1));
+		MapUtils.addValue(source, 1, 1D);
+		assertEquals(source.get(1), Double.valueOf(2));
+	}
+
+	@Test
+	public void testGetRatioValue() {
+		double result = MapUtils.getRatioValue(ratioMap, MathUtils.TEN_THOUSAND, Integer.valueOf(1), Integer.valueOf(4));
+		assertEquals(Double.valueOf(result), Double.valueOf(0.5));
+	}
+
+	@Test
+	public void testGetPermillageValue() {
+		double result = MapUtils.getPermillageValue(ratioMap, Integer.valueOf(1), Integer.valueOf(4), Integer.valueOf(10));
+		assertEquals(Double.valueOf(result), Double.valueOf(5));
+	}
+
+	@Test
+	public void testGetPercentageValue() {
+		double result = MapUtils.getPercentageValue(ratioMap, Integer.valueOf(1), Integer.valueOf(4), Integer.valueOf(10));
+		assertEquals(Double.valueOf(result), Double.valueOf(50));
 	}
 }
