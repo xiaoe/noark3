cb8c012fd24d749305b87c7ddbf8e4dc43637239
小流氓 <176543888@qq.com>
2020-04-08 18:58:00 +0800

    调整noark-redis与noark-game的依赖关系

--- noark-redis/src/main/java/xyz/noark/redis/RedisPublishReportImpl.java
+++ noark-redis/src/main/java/xyz/noark/redis/RedisPublishReportImpl.java
@@ -11,7 +11,7 @@
  * 3.无论你对源代码做出任何修改和改进，版权都归Noark研发团队所有，我们保留所有权利;
  * 4.凡侵犯Noark版权等知识产权的，必依法追究其法律责任，特此郑重法律声明！
  */
-package xyz.noark.game.bi.impl;
+package xyz.noark.redis;
 
 import static xyz.noark.log.LogHelper.logger;
 
@@ -20,7 +20,6 @@ import com.alibaba.fastjson.JSON;
 import xyz.noark.game.bi.AbstractReportService;
 import xyz.noark.game.bi.ReportData;
 import xyz.noark.game.bi.ReportService;
-import xyz.noark.redis.Redis;
 
 /**
  * 利用Redis的订阅发布实现BI数据上报实现
