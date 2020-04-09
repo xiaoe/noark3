c162dcd17ffdb2cfa1364f8f94343967ef8a629f
小流氓 <176543888@qq.com>
2020-04-09 09:54:46 +0800

    优化noark-xml与noark-game的依赖关系

--- noark-game/src/main/java/xyz/noark/game/template/xml/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * XML类型的模板加载实现.
- *
- * @since 3.1
- */
-package xyz.noark.game.template.xml;
---
+++ noark-xml/src/main/java/xyz/noark/xml/XmlTemplateLoader.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright © 2018 www.noark.xyz All Rights Reserved.
+ * 
+ * 感谢您选择Noark框架，希望我们的努力能为您提供一个简单、易用、稳定的服务器端框架 ！
+ * 除非符合Noark许可协议，否则不得使用该文件，您可以下载许可协议文件：
+ * 
+ * 		http://www.noark.xyz/LICENSE
+ *
+ * 1.未经许可，任何公司及个人不得以任何方式或理由对本框架进行修改、使用和传播;
+ * 2.禁止在本项目或任何子项目的基础上发展任何派生版本、修改版本或第三方版本;
+ * 3.无论你对源代码做出任何修改和改进，版权都归Noark研发团队所有，我们保留所有权利;
+ * 4.凡侵犯Noark版权等知识产权的，必依法追究其法律责任，特此郑重法律声明！
+ */
+package xyz.noark.xml;
+
+import java.util.List;
+
+import xyz.noark.game.template.AbstractTemplateLoader;
+
+/**
+ * XML格式的模板加载类.
+ *
+ * @since 3.0
+ * @author 小流氓(176543888@qq.com)
+ */
+public class XmlTemplateLoader extends AbstractTemplateLoader {
+	private final Xml xml;
+
+	public XmlTemplateLoader(String templatePath) {
+		super(templatePath);
+		this.xml = new Xml();
+	}
+
+	public XmlTemplateLoader(String templatePath, String zone) {
+		super(templatePath, zone);
+		this.xml = new Xml();
+	}
+
+	@Override
+	public <T> List<T> loadAll(Class<T> klass) {
+		return xml.loadAll(templatePath, zone, klass);
+	}
+}
