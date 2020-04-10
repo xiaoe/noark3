0d77b75a0a4a27ae7e4c2c4e62cd1f5696c6230a
小流氓 <176543888@qq.com>
2020-04-10 16:55:29 +0800

    新增BooleanUtils

---
+++ noark-core/src/main/java/xyz/noark/core/util/BooleanUtils.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright © 2018 www.noark.xyz All Rights Reserved.
+ * 
+ * 感谢您选择Noark框架，希望我们的努力能为您提供一个简单、易用、稳定的服务器端框架 ！
+ * 除非符合Noark许可协议，否则不得使用该文件，您可以下载许可协议文件：
+ * 
+ * 		http://www.noark.xyz/LICENSE
+ *
+ * 1.未经许可，任何公司及个人不得以任何方式或理由对本框架进行修改、使用和传播;
+ * 2.禁止在本项目或任何子项目的基础上发展任何派生版本、修改版本或第三方版本;
+ * 3.无论你对源代码做出任何修改和改进，版权都归Noark研发团队所有，我们保留所有权利;
+ * 4.凡侵犯Noark版权等知识产权的，必依法追究其法律责任，特此郑重法律声明！
+ */
+package xyz.noark.core.util;
+
+/**
+ * Boolean工具类。
+ *
+ * @since 3.4
+ * @author 小流氓(176543888@qq.com)
+ */
+public class BooleanUtils {
+
+	private BooleanUtils() {}
+
+	/**
+	 * 将一个字符串的配置转化为Boolean值
+	 * <p>
+	 * 
+	 * @see BooleanUtils#toBooleanObject(String)
+	 * @param str 字符串的配置
+	 * @return Boolean值
+	 */
+	public static boolean toBoolean(String str) {
+		return toBooleanObject(str) == Boolean.TRUE;
+	}
+
+	/**
+	 * 将一个字符串的配置转化为Boolean对象
+	 * 
+	 * @param str 字符串的配置
+	 * @return Boolean对象
+	 */
+	public static Boolean toBooleanObject(final String str) {
+		// 字符串为null,直接返回false
+		if (str == null) {
+			return Boolean.FALSE;
+		}
+
+		// 字符串不能使用非0为true的定理，必需明确真的条件
+		switch (str.trim().toLowerCase()) {
+		case "true":
+		case "1":
+		case "yes":
+		case "ok":
+		case "y":
+		case "on":
+		case "是":
+		case "对":
+		case "真":
+		case "正确":
+			return Boolean.TRUE;
+		default:
+			return Boolean.FALSE;
+		}
+	}
+}
