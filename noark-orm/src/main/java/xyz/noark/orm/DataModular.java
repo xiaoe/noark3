7c006024171111302bd771060fbe6a405f386e5f
小流氓 <176543888@qq.com>
2020-04-20 14:47:16 +0800

    重构数据存档的功能实现

--- noark-orm/src/main/java/xyz/noark/orm/DataModular.java
+++ noark-orm/src/main/java/xyz/noark/orm/DataModular.java
@@ -1,9 +1,9 @@
 /*
  * Copyright © 2018 www.noark.xyz All Rights Reserved.
- * 
+ *
  * 感谢您选择Noark框架，希望我们的努力能为您提供一个简单、易用、稳定的服务器端框架 ！
  * 除非符合Noark许可协议，否则不得使用该文件，您可以下载许可协议文件：
- * 
+ *
  * 		http://www.noark.xyz/LICENSE
  *
  * 1.未经许可，任何公司及个人不得以任何方式或理由对本框架进行修改、使用和传播;
@@ -13,68 +13,84 @@
  */
 package xyz.noark.orm;
 
-import static xyz.noark.log.LogHelper.logger;
-
 import xyz.noark.core.Modular;
 import xyz.noark.core.annotation.Autowired;
 import xyz.noark.core.annotation.Component;
 import xyz.noark.core.annotation.Value;
-import xyz.noark.orm.accessor.DataAccessor;
+import xyz.noark.core.util.ClassUtils;
 import xyz.noark.orm.write.AsyncWriteService;
 
 /**
  * 数据存储模块.
  *
- * @since 3.0
  * @author 小流氓[176543888@qq.com]
+ * @since 3.0
  */
 @Component(name = Modular.DATA_MODULAR)
 public class DataModular implements Modular {
-	/** 服务器数据存档间隔，单位：秒，默认值：5分钟 */
-	public static final String DATA_SAVE_INTERVAL = "data.save.interval";
-	/** 服务器数据缓存间隔，单位：秒，默认值：1小时 */
-	public static final String DATA_OFFLINE_INTERVAL = "data.offline.interval";
-	/** 服务器数据每次批量操作的最大数量，默认：256 */
-	public static final String DATA_BATCH_NUM = "data.batch.num";
-	/** 服务器数据存档SQL是否记录日志，默认值：false */
-	public static final String DATA_SQL_LOG_ENABLE = "data.sql.log.enable";
-	/** 服务器数据存档SQL占位符参数是否记录日志（前提取决于{@link DataModular#DATA_SQL_LOG_ENABLE}） */
-	public static final String DATA_SQL_LOG_PARAMETER_ENABLE = "data.sql.log.parameter.enable";
-	/** 服务器数据开启慢查询的时间（单位：ms）,默认为0，不开启 */
-	public static final String DATA_SLOW_QUERY_SQL_MILLIS = "data.slow.query.sql.millis";
-	/** 服务器数据是否智能删除表中多的字段，默认：false */
-	public static final String DATA_AUTO_ALTER_TABLE_DROP_COLUMN = "data.auto.alter.table.drop.column";
-	/** 服务器数据是否智能修正文本字段的长度，默认：true */
-	public static final String DATA_AUTO_ALTER_COLUMN_LENGTH = "data.auto.alter.column.length";
-	/** 服务器数据是否智能转化EMOJI的字段，默认：true */
-	public static final String DATA_AUTO_ALTER_EMOJI_COLUMN = "data.auto.alter.emoji.column";
+    /**
+     * 服务器数据存档间隔，单位：秒，默认值：5分钟
+     */
+    public static final String DATA_SAVE_INTERVAL = "data.save.interval";
+    /**
+     * 服务器数据缓存间隔，单位：秒，默认值：1小时
+     */
+    public static final String DATA_OFFLINE_INTERVAL = "data.offline.interval";
+    /**
+     * 服务器数据每次批量操作的最大数量，默认：256
+     */
+    public static final String DATA_BATCH_NUM = "data.batch.num";
+    /**
+     * 服务器数据存档SQL是否记录日志，默认值：false
+     */
+    public static final String DATA_SQL_LOG_ENABLE = "data.sql.log.enable";
+    /**
+     * 服务器数据存档SQL占位符参数是否记录日志（前提取决于{@link DataModular#DATA_SQL_LOG_ENABLE}）
+     */
+    public static final String DATA_SQL_LOG_PARAMETER_ENABLE = "data.sql.log.parameter.enable";
+    /**
+     * 服务器数据开启慢查询的时间（单位：ms）,默认为0，不开启
+     */
+    public static final String DATA_SLOW_QUERY_SQL_MILLIS = "data.slow.query.sql.millis";
+    /**
+     * 服务器数据是否智能删除表中多的字段，默认：false
+     */
+    public static final String DATA_AUTO_ALTER_TABLE_DROP_COLUMN = "data.auto.alter.table.drop.column";
+    /**
+     * 服务器数据是否智能修正文本字段的长度，默认：true
+     */
+    public static final String DATA_AUTO_ALTER_COLUMN_LENGTH = "data.auto.alter.column.length";
+    /**
+     * 服务器数据是否智能转化EMOJI的字段，默认：true
+     */
+    public static final String DATA_AUTO_ALTER_EMOJI_COLUMN = "data.auto.alter.emoji.column";
 
-	/** 数据存储默认开启下划线命名方式检测 */
-	public static boolean CheckUnderScoreCase = true;
+    /**
+     * 服务器数据异步存档实现类. 默认值：xyz.noark.orm.write.impl.DefaultAsyncWriteServiceImpl
+     */
+    public static final String DATA_ASYNC_WRITER_SERVICE = "data.async.writer.service";
 
-	/** 定时存档间隔 */
-	@Value(DataModular.DATA_SAVE_INTERVAL)
-	private int saveInterval = 300;
-	@Value(DataModular.DATA_OFFLINE_INTERVAL)
-	private int offlineInterval = 3600;
-	@Value(DataModular.DATA_BATCH_NUM)
-	private int batchOperateNum = 256;
+    /**
+     * 数据存储默认开启下划线命名方式检测
+     */
+    public static boolean CheckUnderScoreCase = true;
 
-	@Autowired
-	private DataAccessor dataAccessor;
-	@Autowired
-	private AsyncWriteService asyncWriteService;
+    @Autowired(required = false)
+    private AsyncWriteService asyncWriteService;
+    @Value(DATA_ASYNC_WRITER_SERVICE)
+    private String asyncWriterServiceClass = "xyz.noark.orm.write.impl.DefaultAsyncWriteServiceImpl";
 
-	@Override
-	public void init() {
-		dataAccessor.judgeAccessType();
-		logger.info("初始化数据存储模块，定时存档的时间间隔为 {}秒, 离线玩家在内存中的存活时间为 {}秒", saveInterval, offlineInterval);
-		asyncWriteService.init(saveInterval, offlineInterval, batchOperateNum);
-	}
+    @Override
+    public synchronized void init() {
+        // 如果异步回写服务没有初始化，那就使用默认的配置
+        if (asyncWriteService == null) {
+            asyncWriteService = ClassUtils.newInstance(asyncWriterServiceClass);
+        }
+        asyncWriteService.init();
+    }
 
-	@Override
-	public void destroy() {
-		asyncWriteService.syncFlushAll();
-		asyncWriteService.shutdown();
-	}
+    @Override
+    public void destroy() {
+        asyncWriteService.shutdown();
+    }
 }
---
+++ noark-orm/src/main/java/xyz/noark/orm/write/AsyncWriteContainer.java
@@ -0,0 +1,280 @@
+package xyz.noark.orm.write;
+
+import xyz.noark.core.exception.DataException;
+import xyz.noark.orm.EntityMapping;
+import xyz.noark.orm.accessor.DataAccessor;
+import xyz.noark.orm.write.OperateType;
+import xyz.noark.orm.write.impl.EntityOperate;
+
+import java.io.Serializable;
+import java.util.*;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static xyz.noark.log.LogHelper.logger;
+
+/**
+ * 异步回写容器.
+ *
+ * @author 小流氓[176543888@qq.com]
+ * @since 3.4
+ */
+class AsyncWriteContainer implements Runnable {
+    /**
+     * 分组存档的ID，可能是玩家ID，也可能是其他一些值
+     */
+    private final Serializable groupId;
+    private final DataAccessor dataAccessor;
+    private final int batchOperateNum;
+
+    /**
+     * 当前已修改过的数据
+     */
+    private Map<String, EntityOperate<?>> entityOperates = new HashMap<>();
+    /**
+     * 最终需要保存的数据
+     */
+    private Map<String, EntityOperate<?>> flushOperates;
+
+    private final ReentrantLock dataUpdateLock = new ReentrantLock();
+    private final ReentrantLock dataFlushLock = new ReentrantLock();
+    /**
+     * 记录异步操作的结果，以便有需求时，操纵这个结果
+     */
+    private final ScheduledFuture<?> future;
+
+    AsyncWriteContainer(Serializable groupId, int saveInterval, ScheduledExecutorService scheduledExecutorService, DataAccessor dataAccessor, int batchOperateNum) {
+        this.groupId = groupId;
+        this.dataAccessor = dataAccessor;
+        this.batchOperateNum = batchOperateNum;
+        this.future = scheduledExecutorService.scheduleAtFixedRate(this, saveInterval, saveInterval, TimeUnit.SECONDS);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> EntityOperate<T> getEntityOperate(EntityMapping<T> em, T entity) {
+        String entityId = em.getPrimaryKey(entity);
+        EntityOperate<T> entityOperate = (EntityOperate<T>) entityOperates.get(entityId);
+        if (null == entityOperate) {
+            entityOperate = new EntityOperate<>(entityId, em);
+            entityOperates.put(entityId, entityOperate);
+        }
+        return entityOperate;
+    }
+
+    /**
+     * 保存一个新增的数据
+     */
+    public <T> void insert(EntityMapping<T> em, T entity) {
+        dataUpdateLock.lock();
+        try {
+            EntityOperate<T> entityOperate = getEntityOperate(em, entity);
+            entityOperate.insert(entity);
+        } finally {
+            dataUpdateLock.unlock();
+        }
+    }
+
+    /**
+     * 保存一个修改过的数据.
+     */
+    public <T> void update(EntityMapping<T> em, T entity) {
+        dataUpdateLock.lock();
+        try {
+            EntityOperate<T> entityOperate = getEntityOperate(em, entity);
+            entityOperate.update(entity);
+        } finally {
+            dataUpdateLock.unlock();
+        }
+    }
+
+    /**
+     * 删除一个数据.
+     */
+    public <T> void delete(EntityMapping<T> em, T entity) {
+        dataUpdateLock.lock();
+        try {
+            EntityOperate<T> entityOperate = getEntityOperate(em, entity);
+            boolean deleted = entityOperate.delete(entity);
+            if (deleted) {
+                entityOperates.remove(entityOperate.getId());
+            }
+        } finally {
+            dataUpdateLock.unlock();
+        }
+    }
+
+    private Map<String, EntityOperate<?>> getNewUpdateData() {
+        if (entityOperates.isEmpty()) {
+            return Collections.emptyMap();
+        }
+        Map<String, EntityOperate<?>> updateData;
+        // 如果变更数据集不为空，就先把他拿出来.
+        dataUpdateLock.lock();
+        try {
+            updateData = entityOperates;
+            this.entityOperates = new HashMap<>(32);
+        } finally {
+            dataUpdateLock.unlock();
+        }
+        return updateData;
+    }
+
+    private void mergeFlushData(Map<String, EntityOperate<?>> updateData) {
+        if (flushOperates == null) {
+            flushOperates = updateData;
+        } else {
+            for (Map.Entry<String, EntityOperate<?>> e : updateData.entrySet()) {
+                String id = e.getKey();
+                EntityOperate<?> existOperate = e.getValue();
+                flushOperates.put(id, existOperate);
+            }
+        }
+    }
+
+    /**
+     * 同步式回写数据.
+     */
+    public <T> void syncFlush() {
+        dataFlushLock.lock();
+        try {
+            // 取出最新有过改动的数据
+            Map<String, EntityOperate<?>> updateData = this.getNewUpdateData();
+            // 合并到要回写的数据里
+            this.mergeFlushData(updateData);
+
+            if (flushOperates != null) {
+                try {
+                    if (!flushOperates.isEmpty()) {
+                        logger.info("开始保存数据，groupId={}", groupId);
+
+                        // 数据分组
+                        Map<EntityMapping<T>, EnumMap<OperateType, List<T>>> grouping = new HashMap<>(256);
+                        for (EntityOperate<?> opx : flushOperates.values()) {
+                            @SuppressWarnings("unchecked")
+                            EntityOperate<T> op = (EntityOperate<T>) opx;
+                            // 一种实体类
+                            EnumMap<OperateType, List<T>> category = grouping.computeIfAbsent(op.getEntityMapping(), key -> new EnumMap<>(OperateType.class));
+                            // 删除操作
+                            if (op.isDelete()) {
+                                category.computeIfAbsent(OperateType.DELETE, key -> new LinkedList<>()).add(op.getEntity());
+                            }
+                            // 插入操作
+                            else if (op.isInsert()) {
+                                category.computeIfAbsent(OperateType.INSERT, key -> new LinkedList<>()).add(op.getEntity());
+                            }
+                            // 修改操作
+                            else if (op.isUpdate()) {
+                                category.computeIfAbsent(OperateType.UPDATE, key -> new LinkedList<>()).add(op.getEntity());
+                            }
+                            // 未知操作
+                            else {
+                                throw new DataException("未知的操作实现...");
+                            }
+                        }
+
+                        // 批量存档
+                        for (Map.Entry<EntityMapping<T>, EnumMap<OperateType, List<T>>> e : grouping.entrySet()) {
+                            this.autoOperateEntity(OperateType.DELETE, e.getKey(), e.getValue());
+                            this.autoOperateEntity(OperateType.UPDATE, e.getKey(), e.getValue());
+                            this.autoOperateEntity(OperateType.INSERT, e.getKey(), e.getValue());
+                        }
+                        logger.info("保存数据完成，groupId={}", groupId);
+                    }
+                } finally {
+                    this.flushOperates = null;
+                }
+            }
+        } finally {
+            dataFlushLock.unlock();
+        }
+    }
+
+    /**
+     * 智能分析存档实体对象.
+     *
+     * @param <T>       实体类型
+     * @param type      操作类型
+     * @param em        实体映射
+     * @param entityMap 实体集合
+     */
+    private <T> void autoOperateEntity(OperateType type, EntityMapping<T> em, EnumMap<OperateType, List<T>> entityMap) {
+        List<T> entitys = entityMap.getOrDefault(type, Collections.emptyList());
+        // 只有一个实体有变化
+        if (entitys.size() == 1) {
+            this.operateEntity(type, em, entitys.get(0));
+        }
+        // 批量操作
+        else {
+            // 肯定要分批存档，那还是转化为ArrayList来切割
+            if (entitys.size() > batchOperateNum) {
+                entitys = new ArrayList<>(entitys);
+            }
+            // 分批
+            for (int i = 0, len = (entitys.size() - 1) / batchOperateNum + 1; i < len; i++) {
+                int start = i * batchOperateNum;
+                int end = Math.min(start + batchOperateNum, entitys.size());
+                this.batchOperateEntity(type, em, entitys.subList(start, end));
+            }
+        }
+    }
+
+    private <T> void batchOperateEntity(OperateType type, EntityMapping<T> em, List<T> entitys) {
+        try {
+            switch (type) {
+                case INSERT:
+                    dataAccessor.batchInsert(em, entitys);
+                    break;
+                case DELETE:
+                    dataAccessor.batchDelete(em, entitys);
+                    break;
+                case UPDATE:
+                    dataAccessor.batchUpdate(em, entitys);
+                    break;
+                default:
+                    break;
+            }
+        } catch (Exception exx) {
+            // 批量失败，那就一个一个来吧...
+            for (T entity : entitys) {
+                this.operateEntity(type, em, entity);
+            }
+        }
+
+    }
+
+    private <T> void operateEntity(OperateType type, EntityMapping<T> em, T entity) {
+        try {
+            switch (type) {
+                case INSERT:
+                    dataAccessor.insert(em, entity);
+                    break;
+                case DELETE:
+                    dataAccessor.delete(em, entity);
+                    break;
+                case UPDATE:
+                    dataAccessor.update(em, entity);
+                    break;
+                default:
+                    break;
+            }
+        } catch (Exception exx) {
+            logger.error("操作实体时数据异常，groupId={}{}", groupId, exx);
+            logger.error("操作实体时的异常数据 entity={}", entity);
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            this.syncFlush();
+        } catch (Throwable e) {// 每次保存必需保证定时器不能停了.
+            logger.error("保存个人数据时异常，groupId=" + groupId, e);
+        }
+    }
+
+    public void close() {
+        this.future.cancel(true);
+    }
+}
