7d31e29cddfa0754acbf4b9506024a24d5c5979d
小流氓 <176543888@qq.com>
2020-05-06 10:31:13 +0800

    修复线程执行组上的注释

--- noark-core/src/main/java/xyz/noark/core/annotation/controller/ExecThreadGroup.java
+++ noark-core/src/main/java/xyz/noark/core/annotation/controller/ExecThreadGroup.java
@@ -1,9 +1,9 @@
 /*
  * Copyright © 2018 www.noark.xyz All Rights Reserved.
- * 
+ *
  * 感谢您选择Noark框架，希望我们的努力能为您提供一个简单、易用、稳定的服务器端框架 ！
  * 除非符合Noark许可协议，否则不得使用该文件，您可以下载许可协议文件：
- * 
+ *
  * 		http://www.noark.xyz/LICENSE
  *
  * 1.未经许可，任何公司及个人不得以任何方式或理由对本框架进行修改、使用和传播;
@@ -15,35 +15,38 @@ package xyz.noark.core.annotation.controller;
 
 /**
  * 执行线程组枚举类.
+ * <p>
+ * 线程调度规则：N个线程 处理M个队列，哪个线程空闲了就去处理有任务的队列
+ * </p>
  *
- * @since 3.0
  * @author 小流氓[176543888@qq.com]
+ * @since 3.0
  */
 public enum ExecThreadGroup {
-	/**
-	 * Netty本身的Work线程.
-	 * <p>
-	 * 心跳，或完全没有IO操作的逻辑，直接交给Netty的Work线程处理掉
-	 */
-	NettyThreadGroup,
+    /**
+     * Netty本身的Work线程.
+     * <p>
+     * 心跳，或完全没有IO操作的逻辑，直接交给Netty的Work线程处理掉
+     */
+    NettyThreadGroup,
 
-	/**
-	 * 以玩家ID划分的线程.
-	 * <p>
-	 * 基本就是一个玩家一个线程<br>
-	 */
-	PlayerThreadGroup,
+    /**
+     * 以玩家ID划分的线程.
+     * <p>
+     * 可以理解为一个玩家一个线程<br>
+     */
+    PlayerThreadGroup,
 
-	/**
-	 * 以模块划分的线程.
-	 * <p>
-	 * 基本就是一个Controller一个线程<br>
-	 * 如：登录，世界聊天，公会，排行榜
-	 */
-	ModuleThreadGroup,
+    /**
+     * 以模块划分的线程.
+     * <p>
+     * 可以理解为一个Controller一个线程<br>
+     * 如：登录，世界聊天，公会，排行榜
+     */
+    ModuleThreadGroup,
 
-	/**
-	 * 一种串行执行队列处理线程.
-	 */
-	QueueThreadGroup;
+    /**
+     * 一种串行执行队列处理线程.
+     */
+    QueueThreadGroup;
 }
