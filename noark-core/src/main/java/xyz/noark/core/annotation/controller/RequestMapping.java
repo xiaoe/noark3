3ecb90db104b30133abf80daefa34364308a0f96
小流氓 <176543888@qq.com>
2020-05-20 14:08:37 +0800

    完善RequestMapping注解类的注释

--- noark-core/src/main/java/xyz/noark/core/annotation/controller/RequestMapping.java
+++ noark-core/src/main/java/xyz/noark/core/annotation/controller/RequestMapping.java
@@ -7,6 +7,7 @@ import java.lang.annotation.*;
 /**
  * RequestMapping注解的作用将HTTP请求映射到指定处理方法.
  * <p>
+ * 这里区别Spring的只能在方法上定义，类上面没有实现，感觉没有必要
  *
  * @author 小流氓[176543888@qq.com]
  * @since 3.4
@@ -23,7 +24,23 @@ public @interface RequestMapping {
      */
     String path();
 
+    /**
+     * 允许的请求方式，如果没有指定默认全部允许.
+     * <p>
+     * GET、POST、PUT、DELETE等；
+     * </p>
+     *
+     * @return 允许的请求方式
+     */
     RequestMethod[] method() default {};
 
+    /**
+     * 指定串型执行的队列ID参数的名称.
+     * <p>
+     * 这里的队列ID值是指HTTP请求参数中名称，如果找到名称就执行串型化，没有找到则放弃串型执行
+     * </p>
+     *
+     * @return 串型执行的队列ID参数的名称
+     */
     String queueId() default StringUtils.EMPTY;
 }
