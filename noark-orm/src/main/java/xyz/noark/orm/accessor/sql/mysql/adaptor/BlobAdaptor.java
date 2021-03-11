d343948768a17c70cbe82f67f4387aee41bf3db6
小流氓 <176543888@qq.com>
2021-03-11 13:43:08 +0800

    优化Blob对其他类型的支持

--- noark-orm/src/main/java/xyz/noark/orm/accessor/sql/mysql/adaptor/BlobAdaptor.java
+++ noark-orm/src/main/java/xyz/noark/orm/accessor/sql/mysql/adaptor/BlobAdaptor.java
@@ -13,7 +13,10 @@
  */
 package xyz.noark.orm.accessor.sql.mysql.adaptor;
 
+import com.alibaba.fastjson.JSON;
+import xyz.noark.core.util.CharsetUtils;
 import xyz.noark.core.util.GzipUtils;
+import xyz.noark.core.util.StringUtils;
 import xyz.noark.orm.FieldMapping;
 import xyz.noark.orm.accessor.sql.PreparedStatementProxy;
 
@@ -32,18 +35,33 @@ class BlobAdaptor extends AbstractValueAdaptor<Object> {
 
     @Override
     protected void toPreparedStatement(FieldMapping fm, PreparedStatementProxy pstmt, Object value, int parameterIndex) throws Exception {
+        // 空类型
         if (value == null) {
             pstmt.setNull(parameterIndex, Types.BLOB);
-        } else {
-            byte[] array = (byte[]) value;
-            final int length = array.length;
-            // 当且仅当开启了压缩且数据大于压缩阀值
-            if (fm.isGzip() && length > fm.getGzipThreshold()) {
-                array = GzipUtils.compress(array);
-                logger.debug("Gzip compress. {}->{}", length, array.length);
-            }
-            pstmt.setObject(parameterIndex, array);
+            return;
         }
+
+        byte[] array;
+        //  byte[]
+        if (fm.getFieldClass() == byte[].class) {
+            array = (byte[]) value;
+        }
+        // 字符串
+        else if (fm.getFieldClass() instanceof CharSequence) {
+            array = StringUtils.utf8Bytes((CharSequence) value);
+        }
+        // 其他都要进行序列化
+        else {
+            array = JSON.toJSONBytes(value);
+        }
+
+        final int length = array.length;
+        // 当且仅当开启了压缩且数据大于压缩阀值
+        if (fm.isGzip() && length > fm.getGzipThreshold()) {
+            array = GzipUtils.compress(array);
+            logger.debug("Gzip compress. {}->{}", length, array.length);
+        }
+        pstmt.setObject(parameterIndex, array);
     }
 
     @Override
@@ -60,6 +78,25 @@ class BlobAdaptor extends AbstractValueAdaptor<Object> {
             logger.debug("Gzip uncompress. {}->{}", length, array.length);
         }
 
-        return array;
+        //  byte[]
+        if (fm.getFieldClass() == byte[].class) {
+            return array;
+        }
+        // 字符串
+        else if (fm.getFieldClass() == String.class) {
+            return new String(array, CharsetUtils.CHARSET_UTF_8);
+        }
+        // StringBuilder
+        else if (fm.getFieldClass() == StringBuilder.class) {
+            return new StringBuilder(new String(array, CharsetUtils.CHARSET_UTF_8));
+        }
+        // StringBuffer
+        else if (fm.getFieldClass() == StringBuffer.class) {
+            return new StringBuffer(new String(array, CharsetUtils.CHARSET_UTF_8));
+        }
+        // 其他都要进行序列化
+        else {
+            return JSON.parseObject(array, fm.getFieldClass());
+        }
     }
 }
